8d15f6c148f765df4271a8a90d07c6dfb4653f62
Albert Balbastre-Morte <bulbastre@gmail.com>
2023-08-11 13:10:46 +0200

    untrack everything

--- src/services/getAllPlaylists.ts
+++
@@ -1,8 +0,0 @@
-import { PLAYLISTS } from "../MOCKS/playlists.ts";
-import { Song } from "./interfaces";
-
-export const getAllPlaylists = async (): Promise<Record<string, Song[]>> => {
-  const playlists = await Promise.resolve(PLAYLISTS);
-
-  return playlists;
-};
--- src/src/services/getAllPlaylists.ts
+++
@@ -1,8 +0,0 @@
-import { PLAYLISTS } from "../MOCKS/playlists";
-import { Song } from "./interfaces";
-
-export const getAllPlaylists = async (): Promise<Record<string, Song[]>> => {
-  const playlists = await Promise.resolve(PLAYLISTS);
-
-  return playlists;
-};
